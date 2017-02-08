53f0b4c783eff7abd067c6edcb5cc3e519cb6820
shilpi15gupta <shilpi.gupta@skilrock.com>
2017-02-08 13:51:17 +0530

    multiply

---
+++ src/main/java/GIT_DEMO/GIT_DEMO_1/App.java
@@ -0,0 +1,13 @@
+package GIT_DEMO.GIT_DEMO_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/GIT_DEMO/GIT_DEMO_1/Multiply.java
@@ -0,0 +1,10 @@
+package GIT_DEMO.GIT_DEMO_1;
+
+public class Multiply {
+static int a=4;
+static int b=3;
+
+public static void main(String[] args) {
+	System.out.println(a*b);
+}
+}
